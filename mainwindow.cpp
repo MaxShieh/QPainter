d96fd9a478afeffbfb0df29747e0881400165c10
MaxShieh <zhenzhaoxie@outlook.com>
2021-11-18 08:37:31 +0800

    Calculate Run Once

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,29 +1,44 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "wall.h"
+#include <iostream>
 #include <QtMath>
 #include <QPainter>
 #include <QPaintEvent>
 #include <QDebug>
 
+WallParameterIn_st wallParamIn_st;
+WallParameterOut_st wallParamOut_st;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    //设置输入参数
+    wallParamIn_st.uiWallHeight = 3000;
+    wallParamIn_st.uiWallLength = 2500;
+    wallParamIn_st.uiBrickHeight = 200;
+    wallParamIn_st.uiBrickLength = 600;
+    wallParamIn_st.uiTopJointThickness = 20;
+    wallParamIn_st.uiJointThickness = 5;
+
+    //调用墙体计算函数
+    WallCalculate(wallParamIn_st, wallParamOut_st);
+
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
+    //释放动态内存
+    //delete[] wallParamOut_st.bricks;
+    free(wallParamOut_st.bricks);
     delete ui;
 }
 
 void MainWindow::paintEvent(QPaintEvent *event)   //QPaintEvent *event
 {
     //Q_UNUSED(event);
-
-    QEvent::Type type = event->type();
-    qDebug() << QString(type);
 
     struct aa
     {
@@ -47,28 +62,19 @@ void MainWindow::paintEvent(QPaintEvent *event)   //QPaintEvent *event
     // 绘制文本
     //painter.drawText(rect(), Qt::AlignCenter, "Qt");
 
-    this->resize(this->width(),this->width() * ((float)3000 / 2500));
-    WallParameterIn_st wallParamIn_st;
-    WallParameterOut_st wallParamOut_st;
-    //设置输入参数
-    wallParamIn_st.uiWallHeight = 3000;
-    wallParamIn_st.uiWallLength = 2500;
-    wallParamIn_st.uiBrickHeight = 200;
-    wallParamIn_st.uiBrickLength = 600;
-    wallParamIn_st.uiTopJointThickness = 20;
-    wallParamIn_st.uiJointThickness = 5;
+    //this->resize(this->width(),this->width() * ((float)3000 / 2500));
 
     //计算绘图比例
     float widthRatio = (float)(this->width() - 20) / wallParamIn_st.uiWallLength;
     float heightRatio = (float)(this->height() - 20) / wallParamIn_st.uiWallHeight;
-    //调用墙体计算函数
-    WallCalculate(wallParamIn_st, wallParamOut_st);
 
-    float x(10 + wallParamIn_st.uiJointThickness * widthRatio), y(this->height() - 10 - wallParamIn_st.uiJointThickness * heightRatio);
+    //float x(10 + wallParamIn_st.uiJointThickness * widthRatio), y(this->height() - 10 - wallParamIn_st.uiJointThickness * heightRatio);
     int rank(1);
     int column(1);
-    int width(wallParamIn_st.uiBrickLength);
-    const int height(wallParamIn_st.uiBrickHeight);
+    int x(0);
+    int y(0);
+    int width(0);
+    int height(0);
 
     QPainterPath path;
 //-------------用黑线画出墙体的区域----------------------
@@ -81,60 +87,57 @@ void MainWindow::paintEvent(QPaintEvent *event)   //QPaintEvent *event
 
 //------------------------------------------
     path.clear();
-    while(rank <= wallParamOut_st.uiFullBrickLayer + (wallParamOut_st.uiTopBrickHeight == 0 ? 0 : 1))
+    int brickIndex = 0;
+    bool isLayer2 = false;
+    while(rank <= wallParamOut_st.uiTotalBrickLayer)
     {
-        width = (rank % 2 == 0 && column == 1 ? wallParamOut_st.uiBrokenLen1OfLayer2 : wallParamIn_st.uiBrickLength);
-
-        while (column <= (rank % 2 == 0 ?
-                         wallParamOut_st.uiFullBrickNumOfLayer2 + wallParamOut_st.uiBrokenBrickNumOfLayer2 +1
-                         : wallParamOut_st.uiFullBrickNumOfLayer1 + 1 + (wallParamOut_st.uiBrokenLenOfLayer1 == 0?0:1)) )
+        isLayer2 = (rank % 2 == 0) ? true : false;
+        column = 1;
+        while (column <= (isLayer2 ? wallParamOut_st.uiFullNum_Layer2 +  wallParamOut_st.uiBrokenNum_Layer2 :
+                                    wallParamOut_st.uiFullNum_Layer1 +  wallParamOut_st.uiBrokenNum_Layer1) )
         {
-            path.moveTo(x,y);
+            Brick *pBrick = &wallParamOut_st.bricks[brickIndex];
+            
+            //注意：无符号数必须先强制转化为带符号数才能得到负数
+            x = 10 + pBrick->x * widthRatio;
+            y = this->height() - 10 - pBrick->y * heightRatio;
+            width = pBrick->width * widthRatio;
+            height = -1 * pBrick->height * heightRatio;
             path.setFillRule(Qt::WindingFill);
-
-            if(rank == wallParamOut_st.uiFullBrickLayer + 1)
-                //注意：无符号数必须先强制转化为带符号数才能得到负数
-                path.addRect(x,y,width * widthRatio, -1 *(int)wallParamOut_st.uiTopBrickHeight* heightRatio);
-            else
-                path.addRect(x,y,width * widthRatio, -height * heightRatio);
-
+            path.addRect(x, y, width, height);
+//            std::cout<<10 + pBrick->x * widthRatio <<"," << this->height() - 10 - pBrick->y * widthRatio <<"," <<
+//                            pBrick->width * widthRatio <<"," <<-pBrick->height * heightRatio << std::endl;
             if (rank == 1 && column == 3)
             {
-                a.x = x;
-                a.y = y;
-                a.width = width;
-                a.height = height;
+                a.x = pBrick->x;
+                a.y = pBrick->y;
+                a.width = pBrick->width;
+                a.height = pBrick->height;
             }
-
-            x += (width + wallParamIn_st.uiJointThickness) * widthRatio;
-            column += 1;//列计数加1
-
-            width = wallParamIn_st.uiBrickLength;
-            if(column == (rank % 2 == 0 ? wallParamOut_st.uiFullBrickNumOfLayer2 + wallParamOut_st.uiBrokenBrickNumOfLayer2 + 1
-                          : wallParamOut_st.uiFullBrickNumOfLayer1 + (wallParamOut_st.uiBrokenLenOfLayer1 == 0?0:1) ))
-            {
-                width = (rank % 2 == 0 ? wallParamOut_st.uiBrokenLen2OfLayer2 : wallParamOut_st.uiBrokenLenOfLayer1);
-            }
-            int a =20;
+            column += 1;    //列计数加1
+            brickIndex += 1;
         }
         rank ++;
         column = 1;
-        x = 10 + wallParamIn_st.uiJointThickness * widthRatio;
-        y -= ( height + wallParamIn_st.uiJointThickness) * heightRatio;
     }
 
+//    Brick bricks[75];
+//    for (int i = 0; i < 75; i++)
+//    {
+//        bricks[i] = wallParamOut_st.bricks[i];
+//    }
     painter.setRenderHint(QPainter::Antialiasing, true);
     painter.setPen(Qt::red);
     painter.setBrush(QBrush(Qt::Dense5Pattern));
     painter.drawPath(path);
 
-
-   path.clear();
-   path.moveTo(a.x, a.y);
-   path.addRect(a.x, a.y, a.width * widthRatio, -1 * (int)a.height *heightRatio);
-   painter.setRenderHint(QPainter::Antialiasing, true);
-   painter.setPen(Qt::red);
-   painter.setBrush(QBrush(QColor(0,0,255,80),Qt::SolidPattern));
-   painter.drawPath(path);
+    //高亮一块砖
+    path.clear();
+    path.moveTo(a.x, a.y);
+    path.addRect(10 + a.x * widthRatio, this->height() - 10 - a.y * heightRatio, a.width * widthRatio, -1 * (int)a.height *heightRatio);
+    painter.setRenderHint(QPainter::Antialiasing, true);
+    painter.setPen(Qt::red);
+    painter.setBrush(QBrush(QColor(0,0,255,80),Qt::SolidPattern));
+    painter.drawPath(path);
 
 }
--- wall.h
+++ wall.h
@@ -1,6 +1,9 @@
 #ifndef WALL_H
 #define WALL_H
 
+#include <stdlib.h>
+#include <cstring>
+
 typedef unsigned int uint ;
 
 struct Brick
@@ -34,12 +37,13 @@ struct WallParameterIn_st
 struct WallParameterOut_st
 {
     uint uiFullBrickLayer;	//整砖层数
+    uint uiTotalBrickLayer; //包括顶层非整砖在内的所有砖的层数
     uint uiTopBrickHeight;	//顶砖高度
-    uint uiFullBrickNumOfLayer1;	//第一层整砖数量
-    uint uiBrokenNumOfLayer1;   //第一层非整砖数量
+    uint uiFullNum_Layer1;	//第一层整砖数量
+    uint uiBrokenNum_Layer1;   //第一层非整砖数量
     uint uiBrokenLenOfLayer1;	//第一层非整砖长度
-    uint uiFullBrickNumOfLayer2;	//第二层整砖数量
-    uint uiBrokenBrickNumOfLayer2;	//第二层非整砖数量
+    uint uiFullNum_Layer2;	//第二层整砖数量
+    uint uiBrokenNum_Layer2;	//第二层非整砖数量
     uint uiBrokenLen1OfLayer2;	//第二层第一块非整砖长度
     uint uiBrokenLen2OfLayer2;	//第二层第二块非整砖长度
     Brick *bricks;  //所有砖的数据
@@ -60,55 +64,111 @@ void WallCalculate(const WallParameterIn_st& in, WallParameterOut_st& out)
     //最顶部砌体高度
     out.uiTopBrickHeight = (in.uiWallHeight - in.uiTopJointThickness) % (in.uiBrickHeight + in.uiJointThickness) 
                                     - in.uiJointThickness;
+
+    out.uiTotalBrickLayer = out.uiFullBrickLayer + (out.uiTopBrickHeight == 0 ? 0 : 1);
     out.uiTopBrickHeight = (out.uiTopBrickHeight > 0) ? out.uiTopBrickHeight : 0;   //如果小于0，设置为0
         
     //第一排整砖数量计算
-    out.uiFullBrickNumOfLayer1 = in.uiWallLength / (in.uiBrickLength + in.uiJointThickness);
+    out.uiFullNum_Layer1 = in.uiWallLength / (in.uiBrickLength + in.uiJointThickness);
 
     //第一排非整砖数量
-    out.uiBrokenNumOfLayer1 = 1;
+    out.uiBrokenNum_Layer1 = 1;
 
     //第一排非整砖长度
-    out.uiBrokenLenOfLayer1 = in.uiWallLength - (in.uiBrickLength + in.uiJointThickness) * out.uiFullBrickNumOfLayer1
+    out.uiBrokenLenOfLayer1 = in.uiWallLength - (in.uiBrickLength + in.uiJointThickness) * out.uiFullNum_Layer1
                                  - 2 * in.uiJointThickness;
     out.uiBrokenLenOfLayer1 = (out.uiBrokenLenOfLayer1 > 0) ? out.uiBrokenLenOfLayer1 : 0;   //如果小于0，设置为0
-    uint& len = out.uiBrokenLenOfLayer1;   //不想写这么长的名字，定义一个引用
-    if (len < 80)
-    {
-        out.uiFullBrickNumOfLayer1 -= 1;
-        out.uiBrokenNumOfLayer1 = 2;
-        len = (len + in.uiBrickLength) / 2.0;
-    }
 
     //第二排整砖、非整砖数量
     if (out.uiBrokenLenOfLayer1 < in.uiBrickLength / 3.0)
     {
         //
-        out.uiFullBrickNumOfLayer2 = (out.uiBrokenNumOfLayer1 == 2) ? out.uiFullBrickNumOfLayer1 : out.uiFullBrickNumOfLayer1 - 1;
-        out.uiBrokenBrickNumOfLayer2 = 2;
+        out.uiFullNum_Layer2 = out.uiFullNum_Layer1 - 1;
+        out.uiBrokenNum_Layer2 = 2;
         out.uiBrokenLen1OfLayer2 = in.uiBrickLength / 2.0; //第二排第一块非整砖长度
     }
     else if( in.uiBrickLength / 3.0 * 2.0> out.uiBrokenLenOfLayer1 && out.uiBrokenLenOfLayer1 > in.uiBrickLength / 3.0 )
     {
-        out.uiFullBrickNumOfLayer2 = out.uiFullBrickNumOfLayer1;
-        out.uiBrokenBrickNumOfLayer2 = 1;
+        out.uiFullNum_Layer2 = out.uiFullNum_Layer1;
+        out.uiBrokenNum_Layer2 = 1;
         out.uiBrokenLen1OfLayer2 = out.uiBrokenLenOfLayer1;    //第二排第一块非整砖长度
     }
     else
     {
-        out.uiFullBrickNumOfLayer2 = out.uiFullBrickNumOfLayer1 - 1;
-        out.uiBrokenBrickNumOfLayer2 = 2;
+        out.uiFullNum_Layer2 = out.uiFullNum_Layer1 - 1;
+        out.uiBrokenNum_Layer2 = 2;
         out.uiBrokenLen1OfLayer2 = in.uiBrickLength / 2.0; //第二排第一块非整砖长度
     }
 
-
     //第二排第二块非整砖长度
-    long temp = in.uiWallLength - (in.uiBrickLength + in.uiJointThickness) * out.uiFullBrickNumOfLayer2 
+    long temp = (long)(in.uiWallLength - (in.uiBrickLength + in.uiJointThickness) * out.uiFullNum_Layer2 
                     - (out.uiBrokenLen1OfLayer2 + in.uiJointThickness)
-                    - 2 * in.uiJointThickness;
-    if (len < 0)
+                    - 2 * in.uiJointThickness);
+    if (temp < 0)
         out.uiBrokenLen2OfLayer2 = 0;
     else
-        out.uiBrokenLen2OfLayer2 = len;
+        out.uiBrokenLen2OfLayer2 = temp;
+
+////-----------------考虑最后一块砖小于80的情况---------------
+//    uint& len = out.uiBrokenLenOfLayer1;   //不想写这么长的名字，定义一个引用
+//    if (len < 80)
+//    {
+//        out.uiFullNum_Layer1 -= 1;
+//        out.uiBrokenNum_Layer1 = 2;
+//        len = (len + in.uiBrickLength) / 2.0;
+//    }
+
+    //-------------------记录所有砖块的信息-----------------------
+    int totalBricks_Layer1 = out.uiFullNum_Layer1 + out.uiBrokenNum_Layer1;
+    int totalBricks_Layer2 = out.uiFullNum_Layer2 + out.uiBrokenNum_Layer2;
+    //如果有顶层砖，也需包含上 
+    int totalBricks = out.uiTotalBrickLayer / 2 * (totalBricks_Layer1 + totalBricks_Layer2)
+                                + out.uiTotalBrickLayer % 2 * totalBricks_Layer1;                           
+
+    //创建动态内存空间-------------------------------记住以后要delete[]
+    //out.bricks = new Brick[totalBricks];
+    out.bricks = (Brick*)malloc(totalBricks * sizeof(Brick));
+    memset(out.bricks, 0, totalBricks * sizeof(Brick));
+
+    float x(in.uiJointThickness), y(in.uiJointThickness);
+    int rank(1), column(1);
+    int width(in.uiBrickLength);
+    const int height(in.uiBrickHeight);
+    int brickIndex = 0;
+
+    while(rank <= out.uiTotalBrickLayer)
+    {
+        width = (rank % 2 == 0 && column == 1 ? out.uiBrokenLen1OfLayer2 : in.uiBrickLength);
+
+        while (column <= (rank % 2 == 0 ? totalBricks_Layer2 : totalBricks_Layer1) )
+        {
+            Brick *pBrick = &out.bricks[brickIndex];
+            pBrick->x = x;
+            pBrick->y = y;
+            pBrick->width = width;
+            pBrick->height = (rank == out.uiFullBrickLayer + 1) ? (int)out.uiTopBrickHeight : (int)height;
+            pBrick->rank = rank;
+            pBrick->column = column;
+
+            x += width + in.uiJointThickness;
+            column += 1;    //列计数加1
+            brickIndex += 1;    //下标加1
+
+            width = in.uiBrickLength;   //从第二块砖开始（砖的长度为整砖长度）
+            if (rank % 2 == 0)
+            {
+                width = (column > out.uiFullNum_Layer2 + 1) ? out.uiBrokenLen2OfLayer2 : width;
+            }
+            else
+            {
+                width = column > out.uiFullNum_Layer1 ? out.uiBrokenLenOfLayer1 : width;
+            }
+        }
+
+        rank ++;    //轮询完一行后，行计数+1
+        column = 1; //轮询完一行后，列计数重置为1
+        x = in.uiJointThickness; //轮询完一行后，第一块砖的坐标重置
+        y += ( height + in.uiJointThickness);
+    }
 }
 #endif // WALL_H
